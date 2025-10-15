efb7577dd2b54e144268a0e28f6426703ff710df
Antoine LALA <a.lala@formation-cci12.fr>
2025-10-15 09:50:04 +0200

    test

---
+++ src/test/java/ace/projetprogpro/ProjetProgProApplicationTests.java
@@ -0,0 +1,13 @@
+package ace.projetprogpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetProgProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
