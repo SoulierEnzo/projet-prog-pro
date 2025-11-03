39b305fc4f0bd3057c18c8aab6b4f48a2cad2852
lala-antoine <lalaa@3il.fr>
2025-11-03 20:51:13 +0100

    encoding v1

--- application/lancement_windows.bat
+++ application/lancement_windows.bat
@@ -5,6 +5,6 @@ echo === Lancement du Docker ===
 docker-compose up -d
 
 echo === Lancement de l'application Java ===
-java -jar interview-simulator-0.0.1-SNAPSHOT.jar
+java -Dfile.encoding=UTF-8 -jar interview-simulator-0.0.1-SNAPSHOT.jar
 
 pause
